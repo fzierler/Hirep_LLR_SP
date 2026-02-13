a24413291f63be684e0a11f1138d1697befd327e
Fabian Zierler <fabian.zierler@a1.net>
2026-02-13 08:04:00 +0100

    fix c99 errors with recent intel compilers

--- Include/gdbhook.h
+++ Include/gdbhook.h
@@ -1,8 +1,10 @@
 #ifndef __DEBUG_H_
 #define __DEBUG_H_
+#include <stddef.h>
 
 // To be able to run GDB on MPI jobs, as desribed in
 // https://www.open-mpi.org/faq/?category=debugging#serial-debuggers
+int gethostname(char *name, size_t size);
 void gdb_hook();
 
 #endif // __DEBUG_H_
